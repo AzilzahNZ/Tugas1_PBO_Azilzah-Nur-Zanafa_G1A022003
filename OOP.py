ad2a0104c28bd7f387c68fce2b88bbe744241aac
AzilzahNZ <131631566+AzilzahNZ@users.noreply.github.com>
2023-04-24 16:30:31 +0700

    Create OOP.py

---
+++ OOP.py
@@ -0,0 +1,6 @@
+class luasSegitiga:
+    def luas(self, a, t):
+        return 0.5 * a * t
+
+hitung = luasSegitiga()
+print("Luas segitiga siku-siku dengan ukuran alas 6 cm dan tinggi 8 cm adalah sebesar ", hitung.luas(6, 8) , "cm")
